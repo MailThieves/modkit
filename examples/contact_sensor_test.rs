c508e63cf0edfd574bb278b932356dd53e640d30
llamicron <llamicron@gmail.com>
2023-03-27 13:50:50 -0500

    example edits

--- examples/contact_sensor_test.rs
+++ examples/contact_sensor_test.rs
@@ -12,15 +12,8 @@ fn main() {
 
     info!("Contact sensor connected: {:?}", cs.connected());
 
-    // let pin = Gpio::new()
-    //     .expect("GPIO connection")
-    //     .get(18)
-    //     .expect("Pin connection")
-    //     .into_input();
-
     loop {
         info!("State = {:?}", cs.poll());
-        // info!("Pin is low? {}", pin.is_low());
         sleep(Duration::from_millis(200));
     }
 }
