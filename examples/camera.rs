53362fdbd296495461a10d71ec8c0e4fbc699721
llamicron <llamicron@gmail.com>
2023-03-28 00:01:26 -0500

    Removes rascam and updates camera example

--- examples/camera.rs
+++ examples/camera.rs
@@ -1,9 +1,8 @@
 #![allow(unused)]
 use log::*;
+use std::process::Command;
 use std::thread::sleep;
 use std::time::Duration;
-
-use rascam;
 
 use modkit::prelude::*;
 
@@ -12,27 +11,18 @@ fn main() {
     error!("Be sure to use info log level");
     info!("Starting camera example");
 
-    let info = rascam::info().expect("camera info");
+    info!("Running the raspistill command");
 
-    if info.cameras.len() < 1 {
-        error!("Found 0 cameras. Exiting");
-        std::process::exit(1);
+    let output = Command::new("raspistill")
+        .args(["-o", "test.jpg"])
+        .output();
+
+    match output {
+        Ok(out) => {
+            error!("{:#?}", out.stderr);
+            info!("{:#?}", out.stdout);
+        },
+        Err(e) => error!("couldn't use the raspistill command. Are you on the pi? {e}")
     }
 
-    info!("{}", info);
-
-    if let Some(cam_info) = info.cameras.get(0) {
-        let mut camera = rascam::SimpleCamera::new(cam_info.clone()).expect("opening camera");
-        camera.activate().expect("activate camera");
-
-        sleep(Duration::from_secs(2));
-
-        let bytes = camera.take_one().expect("taking a picture");
-        File::create("image.jpg")
-            .expect("creating file")
-            .write_all(&bytes)
-            .expect("write to file");
-
-        info!("Saved image as image.jpg");
-    }
 }
