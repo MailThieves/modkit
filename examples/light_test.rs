7b64fe8aa78b17f7b48bcdc87d26564df157ae2f
llamicron <llamicron@gmail.com>
2023-03-29 01:26:04 -0500

    Updates light test

--- examples/light_test.rs
+++ examples/light_test.rs
@@ -9,24 +9,12 @@ fn main() {
 
     info!("Starting light test");
 
-    light::set(true).expect("set light");
-    sleep(Duration::from_millis(100));
-    light::set(false).expect("set light");
-    sleep(Duration::from_millis(100));
-    light::set(true).expect("set light");
-    sleep(Duration::from_millis(100));
-
     let mut state = true;
     loop {
-        info!("Setting light to state {}", state);
         light::set(state).expect("set light");
         info!("Light is on? {:?}", light::is_on());
-        sleep(Duration::from_secs(2));
-
-        info!("Toggling light");
-        light::toggle().expect("toggle");
-
-        sleep(Duration::from_secs(2));
+        
         state = !state;
+        sleep(Duration::from_secs(2));
     }
 }
