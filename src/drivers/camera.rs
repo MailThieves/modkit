cb77f085372a9aca7be0b75be58f9cdb89c97179
llamicron <llamicron@gmail.com>
2023-03-29 01:44:19 -0500

    Refactors, and adds capture_img script

--- src/drivers/camera.rs
+++ src/drivers/camera.rs
@@ -1,53 +1,119 @@
-use std::path::{Path, PathBuf};
+#[cfg(feature = "hardware")]
+pub mod camera {
+    use std::path::{Path, PathBuf};
 
-use image::{ImageBuffer, RgbImage};
-use log::*;
+    use image::{ImageBuffer, RgbImage};
+    use log::*;
 
-use super::DeviceError;
+    use super::super::light::light;
+    use super::super::DeviceError;
 
-fn capture_into<P: AsRef<Path>>(path: P) -> Result<PathBuf, DeviceError> {
-    let dir_path: PathBuf = path.as_ref().to_path_buf();
+    pub fn capture_into<P: AsRef<Path>>(path: P) -> Result<PathBuf, DeviceError> {
+        trace!("Turning light on to capture image");
+        light::set(true)?;
 
-    // Make sure the dir exists
-    if !dir_path.exists() {
-        error!(
-            "Path {} doesn't exist. Please pass a valid directory.",
-            dir_path.display()
-        );
+        let dir_path: PathBuf = path.as_ref().to_path_buf();
 
-        return Err(DeviceError::IoError(format!(
-            "path {} does not exist",
-            dir_path.display()
-        )));
+        // Make sure the dir exists
+        if !dir_path.exists() {
+            error!(
+                "Path {} doesn't exist. Please pass a valid directory.",
+                dir_path.display()
+            );
+
+            return Err(DeviceError::IoError(format!(
+                "path {} does not exist",
+                dir_path.display()
+            )));
+        }
+
+        // And make sure it's actually a dir
+        if !dir_path.is_dir() {
+            error!("Path {} is not a directory", dir_path.display());
+            return Err(DeviceError::IoError(format!(
+                "path {} is not a directory",
+                dir_path.display()
+            )));
+        }
+
+        // Create a file name within the dir
+        let mut img_path = PathBuf::from(dir_path);
+        img_path.push(format!("{}.jpg", chrono::Utc::now().timestamp()));
+
+        // Generate an image and save it
+        let mut img: RgbImage = ImageBuffer::new(50, 50);
+        *img.get_pixel_mut(25, 25) = image::Rgb([255, 255, 255]);
+
+        trace!("Writing captured image to {}", img_path.display());
+        img.save(&img_path)?;
+
+        trace!("Turning light off after image capture");
+        light::set(false)?;
+        // And return the path
+        Ok(img_path)
     }
-
-    // And make sure it's actually a dir
-    if !dir_path.is_dir() {
-        error!("Path {} is not a directory", dir_path.display());
-        return Err(DeviceError::IoError(format!(
-            "path {} is not a directory",
-            dir_path.display()
-        )));
-    }
-
-    // Create a file name within the dir
-    let mut img_path = PathBuf::from(dir_path);
-    img_path.push(format!("{}.jpg", chrono::Utc::now().timestamp()));
-    
-    // Generate an image and save it
-    let mut img: RgbImage = ImageBuffer::new(50, 50);
-    *img.get_pixel_mut(25, 25) = image::Rgb([255, 255, 255]);
-
-    trace!("Writing captured image to {}", img_path.display());
-    img.save(&img_path)?;
-
-    // And return the path
-    Ok(img_path)
 }
 
+#[cfg(not(feature = "hardware"))]
+pub mod camera {
+    use std::path::{Path, PathBuf};
+
+    use image::{ImageBuffer, RgbImage};
+    use log::*;
+
+    use super::super::light::light;
+    use super::super::DeviceError;
+
+    pub fn capture_into<P: AsRef<Path>>(path: P) -> Result<PathBuf, DeviceError> {
+        trace!("Turning light on to capture image");
+        light::set(true)?;
+
+        let dir_path: PathBuf = path.as_ref().to_path_buf();
+
+        // Make sure the dir exists
+        if !dir_path.exists() {
+            error!(
+                "Path {} doesn't exist. Please pass a valid directory.",
+                dir_path.display()
+            );
+
+            return Err(DeviceError::IoError(format!(
+                "path {} does not exist",
+                dir_path.display()
+            )));
+        }
+
+        // And make sure it's actually a dir
+        if !dir_path.is_dir() {
+            error!("Path {} is not a directory", dir_path.display());
+            return Err(DeviceError::IoError(format!(
+                "path {} is not a directory",
+                dir_path.display()
+            )));
+        }
+
+        // Create a file name within the dir
+        let mut img_path = PathBuf::from(dir_path);
+        img_path.push(format!("{}.jpg", chrono::Utc::now().timestamp()));
+
+        // Generate an image and save it
+        let mut img: RgbImage = ImageBuffer::new(50, 50);
+        *img.get_pixel_mut(25, 25) = image::Rgb([255, 255, 255]);
+
+        trace!("Writing captured image to {}", img_path.display());
+        img.save(&img_path)?;
+
+        trace!("Turning light off after image capture");
+        light::set(false)?;
+        // And return the path
+        Ok(img_path)
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
+
     use super::*;
 
     #[test]
@@ -58,11 +124,10 @@ mod tests {
         }
         assert!(dir.exists());
 
-        let file_path_res = capture_into(&dir);
+        let file_path_res = camera::capture_into(&dir);
         assert!(file_path_res.is_ok());
         let file_path = file_path_res.unwrap();
         assert_eq!(file_path.extension().unwrap(), "jpg");
-
 
         std::fs::remove_dir_all("./tmp").unwrap();
     }
--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod prelude {
     pub use crate::drivers::DeviceError;
     pub use crate::drivers::contact_sensor::ContactSensor;
     pub use crate::drivers::light::light;
+    pub use crate::drivers::camera::camera;
 }
 
 // Only used in main()
