5b0f4f4538e02cdf6db9c28e49b0c6defd175f5a
llamicron <llamicron@gmail.com>
2023-04-05 15:31:13 -0500

    Changes default img location

--- src/defaults.rs
+++ src/defaults.rs
@@ -1,7 +1,11 @@
 use std::env::var;
 
 pub fn img_dir() -> String {
-    var("MODKIT_IMG_DIR").unwrap_or(String::from("./img"))
+    // This only runs in one environment, we can safely
+    // assume that it won't panic
+    let mut default_dir = home::home_dir().unwrap();
+    default_dir.push("modkit_images");
+    var("MODKIT_IMG_DIR").unwrap_or(format!("{}", default_dir.display()))
 }
 
 pub fn light_gpio_pins() -> [u8; 4] {
